5f181a674ee9c9e8ec819847279941fd8369cc2e
hrach333 <hrach333@live.ru>
2016-03-25 01:33:54 +0300

    now file
    now file

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define("HOST","localhost");
+define("USER","root");
+define("PASS","");
+define("DB","minicms");
+define("HOMEURL","http://projekt.dev");
+define("THEME","urok");
+define("THEMEURL",HOMEURL."/theme/".THEME);
