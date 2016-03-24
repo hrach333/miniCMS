f6e676c4f4bfcb71c47b69ac9b5dd2b337b941b1
hrach333 <hrach333@live.ru>
2016-03-25 02:21:10 +0300

    Revert "Revert "Revert "Revert "now file""""
    This reverts commit b4e8e275d7355cae26607a0c2da5c8e3f73cfea8.

---
+++ controller/pageController.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'controller.php';
+class pageController extends controller{
+	public function __construct($model){
+		$this->model($model);
+	}
+	public function getPage(){
+		
+	}
+}
