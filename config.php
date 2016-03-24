2f1bdd2de6d18bfdcef8751c7c4821338fe412cc
hrach333 <hrach333@live.ru>
2016-03-25 01:35:20 +0300

    Revert "now file"
    This reverts commit 5f181a674ee9c9e8ec819847279941fd8369cc2e.

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("HOST","localhost");
-define("USER","root");
-define("PASS","");
-define("DB","minicms");
-define("HOMEURL","http://projekt.dev");
-define("THEME","urok");
-define("THEMEURL",HOMEURL."/theme/".THEME);
