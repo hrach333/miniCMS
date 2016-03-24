b4e8e275d7355cae26607a0c2da5c8e3f73cfea8
hrach333 <hrach333@live.ru>
2016-03-25 02:20:19 +0300

    Revert "Revert "Revert "now file"""
    This reverts commit f4e4b80d6ccdf0569c41097e8c2919de1c83955d.

--- function/function.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function getID(){
-		if(isset($_GET['id'])){
-			return $id=$_GET['id'];
-		}else{
-			return $id=1;
-		}
-	}
--- model/pageModel.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'model.php';
-class pageModel extends model{
-	function __construct(){
-
-		}
-}
